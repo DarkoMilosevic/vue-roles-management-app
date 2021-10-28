5f8eb5246280f15d4e760ed1ca0e4518df15f988
Darko <Darko@DESKTOP-UDAI6HC>
2021-10-28 22:53:37 +0200

    format fix

--- src/views/UserRolesDashboard.vue
+++ src/views/UserRolesDashboard.vue
@@ -1,17 +1,17 @@
 <template>
-	<v-container>
-		<h1 class="container__heading">User Roles Management</h1>
+    <v-container>
+        <h1 class="container__heading">User Roles Management</h1>
 
-			<v-row dense>
-				<v-col cols="9">
-					<UserRolesFilter />
-				</v-col>
-					<v-col cols="3" class="d-flex align-end justify-end">
-					<v-btn color="primary" to="/role-form/create">Create new role</v-btn>
-				</v-col>
-			</v-row>
-		<UserRoleCard />
-	</v-container>
+        <v-row dense>
+            <v-col cols="9">
+                <UserRolesFilter />
+            </v-col>
+                <v-col cols="3" class="d-flex align-end justify-end">
+                <v-btn color="primary" to="/role-form/create">Create new role</v-btn>
+            </v-col>
+        </v-row>
+        <UserRoleCard />
+    </v-container>
 </template>
 
 <script>
@@ -20,9 +20,9 @@ import UserRoleCard from '@/components/UserRoleCard';
 
 export default {
 name: 'user-roles-dashboard',
-	components: {
-		UserRolesFilter,
-		UserRoleCard
-	}
+    components: {
+        UserRolesFilter,
+        UserRoleCard
+    }
 };
 </script>
